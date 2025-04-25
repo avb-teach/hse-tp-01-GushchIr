f2c173df78e2ab5016ea7110cb24b93536d325f2
GushchIr <minoriwae@gmail.com>
2025-04-26 00:38:28 +0300

    check1

--- collect_files.sh
+++ collect_files.sh
@@ -3,3 +3,17 @@
 input_dir="$1"
 output_dir="$2"
 max_depth=""
+
+copy_files() {
+    local src="$1"
+    
+    for item in "$src"/*; do
+        if [ -f "$item" ]; then
+            cp "$item" "$output_dir/"
+        elif [ -d "$item" ]; then
+            copy_files "$item"
+        fi
+    done
+}
+
+copy_files "$input_dir"

01f3e3bf51426603c39228f9ed03179ec1d051b4
GushchIr <minoriwae@gmail.com>
2025-04-27 18:45:05 +0300

    check

--- collect_files.sh
+++ collect_files.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-input_dir = "$1"
-output_dir = "$2"
+input_dir="$1"
+output_dir="$2"
 
 give_name() {
-    local way_to_file = "$1"
-    local file_name = $(basename "$way_to_file")
-    local name = "${file_name%.*}"
-    local extension = "${file_name##*.}"
-    local c = 1
-    local new_name = "$file_name"
+    local way_to_file="$1"
+    local file_name=$(basename "$way_to_file")
+    local name="${file_name%.*}"
+    local extension="${file_name##*.}"
+    local c=1
+    local new_name="$file_name"
     while [ -e "$output_dir/$new_name" ]; do
         new_name="${name}_${c}.${extension}"
-        c = $((c + 1))
+        c=$((c + 1))
     done
 
     echo "$new_name"
 }
 
 copy_files() { 
-    local src = "$1"
+    local src="$1"
     
     for item in "$src"/*; do
         if [ -f "$item" ]; then
-            name = $(give_name "$item")
+            name=$(give_name "$item")
             cp "$item" "$output_dir/$name"
         elif [ -d "$item" ]; then
             copy_files "$item"
